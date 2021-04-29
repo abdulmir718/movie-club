a834e4f4725aa0996e5eadc824cb608a00cc4a3b
abdul-mir <abdul.w.mir@outlook.com>
2021-04-29 13:45:46 -0500

    Refactoring featured movies functionality/Starting on search functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Header from "./components/Header";
 import MovieList from "./components/MovieList";
 import "./App.css"
 
-const featuredApi = `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.MOVIE_CLUB_API_KEY}`;
+const featuredApi = `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_MOVIE_CLUB_API_KEY}`;
 
 
-const searchApi = `https://api.themoviedb.org/3/search/collection?api_key=${process.env.MOVIE_CLUB_API_KEY}&language=en-US&query=`;
+const searchApi = `https://api.themoviedb.org/3/search/collection?api_key=${process.env.REACT_APP_MOVIE_CLUB_API_KEY}&language=en-US&query=`;
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     fetch(featuredApi)
@@ -19,12 +20,31 @@ function App() {
     })
     .catch(console.error);
   }, []);
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(searchApi + searchValue)
+    .then((res) => res.json())
+    .then((data) => {
+      setMovies(data.results);
+    })
+    .catch(console.error);
+  };
+
+  const handleOnChange = (event) => {
+    setSearchValue(event.target.value)
+  }
+
   return (
     <div className="App">
-      
+
       <Header />
 
-      <div className = "featuredMovies">
+
+      
+      <div className = "movieList">
+      <h2>Featured Movies</h2>
         {movies.length > 0 &&
         movies.map((movie) => <MovieList key = {movie.id} {...movie} /> )}
       </div>
