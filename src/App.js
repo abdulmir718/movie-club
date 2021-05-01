598295d6990efebaf4541f0fbe6acf2c75000dc1
abdul-mir <abdul.w.mir@outlook.com>
2021-05-01 03:27:58 -0500

    Completed search functionalit/Refactoring

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import "./App.css"
 const featuredApi = `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_MOVIE_CLUB_API_KEY}`;
 
 
-const searchApi = `https://api.themoviedb.org/3/search/collection?api_key=${process.env.REACT_APP_MOVIE_CLUB_API_KEY}&language=en-US&query=`;
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
+  console.log("movies", movies)
 
   useEffect(() => {
     fetch(featuredApi)
@@ -21,32 +20,20 @@ function App() {
     .catch(console.error);
   }, []);
 
-  const handleOnSubmit = (event) => {
-    event.preventDefault();
-
-    fetch(searchApi + searchValue)
-    .then((res) => res.json())
-    .then((data) => {
-      setMovies(data.results);
-    })
-    .catch(console.error);
-  };
-
-  const handleOnChange = (event) => {
-    setSearchValue(event.target.value)
-  }
+  
 
   return (
     <div className="App">
 
-      <Header />
-
+      <div className = "header">
+      <Header setMovies = {setMovies}/>
+      </div>
 
       
       <div className = "movieList">
       <h2>Featured Movies</h2>
-        {movies.length > 0 &&
-        movies.map((movie) => <MovieList key = {movie.id} {...movie} /> )}
+        {movies &&
+        movies.map((movie) => <MovieList  key = {movie.id} {...movie} /> )}
       </div>
 
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 
-function Header(props) {
-    return (
-        <div>
 
-            <header className = "head">
-            <h1 className = "title">Movie Club </h1>
-            <div className = "search">
-            <input className = "searchBar" type = "text" placeholder = "Search Movies..." /> 
-            <button>Search</button>
-            </div>
-            </header>
-            
-        </div>
-    );
+const searchApi = `https://api.themoviedb.org/3/search/movie?language=en-US&api_key=${process.env.REACT_APP_MOVIE_CLUB_API_KEY}&language=en-US&query=`;
+
+
+function Header({setMovies}) {
+
+    const [searchValue, setSearchValue] = useState("");
+    const [submitValue, setSubmitValue]= useState("");
+
+    useEffect(() => {
+        fetch(searchApi + submitValue)
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+        .then(data => {setMovies(data.results)
+        console.log(data.results)})
+      }, [submitValue, setMovies])
+      
+      function handleForm (event) {
+        event.preventDefault();
+        setSubmitValue(searchValue);
+      }
+    
+      const handleOnChange = (event) => {
+        setSearchValue(event.target.value)
+      }
+
+  return (
+    <div>
+      <header className="head">
+        <h1 className="title">Movie Club</h1>
+        <form className="search" onSubmit={ handleForm }>
+          <input
+            className="searchBar"
+            type="text"
+            placeholder="Search Movies..."
+            onChange={handleOnChange}
+            value={searchValue}
+          />
+          <button>Search</button>
+        </form>
+      </header>
+      <h2>{submitValue}</h2>
+    </div>
+  );
 }
 
 export default Header;
